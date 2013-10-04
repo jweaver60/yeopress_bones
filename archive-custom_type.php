99f04c8f3526a74dcff5b291eaa6f7f9f0cc3f62
Dustin Armstrong <dustin@dustinarmstrong.com>
2013-10-04 10:04:15 -0400

    Updated to foundation grid system

--- archive-custom_type.php
+++ archive-custom_type.php
@@ -4,7 +4,7 @@
 
 				<div id="inner-content" class="wrap clearfix">
 
-						<div id="main" class="eightcol first clearfix" role="main">
+						<div id="main" class="small-12 large-8 columns first clearfix" role="main">
 
 						<h1 class="archive-title h2"><?php post_type_archive_title(); ?></h1>
 
